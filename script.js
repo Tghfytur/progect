3a713c3adbc2c4c01ac240b65a17c416b2cf67d0
Tghfytur <128831346+Tghfytur@users.noreply.github.com>
2023-05-07 17:06:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var dates = {
+    "1600" :[" Битва при Сэкигахара.","началось извержение вулкана Перу","Перенесение столицы Японии  в Эдо","Якопо Пери написал оперу «Эвридика»","Иштеряк становится бием Ногайской орды"]
+  }
+
+
+var textInput = document.querySelector('input')
+var button = document.querySelector('button')
+var events = document.querySelectorAll('p') 
+var counter = 0
+
+function onclick (){
+  if (textInput.value in dates) {
+    events.forEach(function(event) {
+      event.textContent = dates[textInput.value][counter]
+      counter++
+    })
+  }
+}
+
+button.addEventListener('click', onclick)
